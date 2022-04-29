334509d681106fb3606b63dbd031e1f8ca3cb308
cbind181 <33586994+cbind181@users.noreply.github.com>
2022-04-29 14:06:20 -0500

    Add files via upload
    Initial Commit

---
+++ aes.py
@@ -0,0 +1,99 @@
+# This is a simple program to utilize AES CBC in Python 3
+# pre-requisit: `pip install pycryptodome`
+#
+# Authors: Cody Binder & Calen Olsen
+# Date: 2022-04-25
+#
+# MIT License
+#
+# Copyright (c) [2022] [Cody Binder, Calen Olsen]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from Crypto import Random
+from Crypto.Cipher import AES
+import os
+import os.path
+from os import listdir
+from os.path import isfile, join
+import time
+
+class Encryptor:
+    def __init__(self, key):
+        self.key = key
+
+    def pad(self, s):
+        return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+
+    def encrypt(self, message, key, key_size=256):
+        message = self.pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        return iv + cipher.encrypt(message)
+
+    def encrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            plaintext = fo.read()
+        enc = self.encrypt(plaintext, self.key)
+        with open(file_name + ".enc", 'wb') as fo:
+            fo.write(enc)
+        os.remove(file_name)
+
+    def decrypt(self, ciphertext, key):
+        iv = ciphertext[:AES.block_size]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+        return plaintext.rstrip(b"\0")
+
+    def decrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            ciphertext = fo.read()
+        dec = self.decrypt(ciphertext, self.key)
+        with open(file_name[:-4], 'wb') as fo:
+            fo.write(dec)
+        os.remove(file_name)
+
+
+key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
+enc = Encryptor(key)
+clear = lambda: os.system('cls')
+
+encryptTimes = []
+decryptTimes = []
+
+for i in range(0,20):
+
+    start = time.time()
+    enc.encrypt_file("msg.txt")
+    end = time.time()
+    print("encrypt time: ", end - start)
+    encryptTimes.append(end - start)
+
+    start = time.time()
+    enc.decrypt_file("msg.txt.enc")
+    end = time.time()
+    print("decrypt time: ", end - start)
+    decryptTimes.append(end - start)
+
+print("average encrypt time: ", sum(encryptTimes)/len(encryptTimes))
+print("average decrypt time: ", sum(decryptTimes)/len(decryptTimes))
+
+print("best encrypt time: ", min(encryptTimes))
+print("best decrypt time: ", min(decryptTimes))
+
