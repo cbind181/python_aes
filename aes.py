0c7448738728e244c26146e5bb331fd6edda238b
cbind181 <33586994+cbind181@users.noreply.github.com>
2022-05-11 15:47:25 -0500

    Update aes.py

--- aes.py
+++ aes.py
@@ -6,17 +6,22 @@ from os import listdir
 from os.path import isfile, join
 import time
 
+# Keep lists of times to 
 encryptTimes = []
 decryptTimes = []
 
+
+# Encryption class handles all AES functionality
 class Encryptor:
     
     def __init__(self, key):
         self.key = key
 
+    # generates message padding
     def pad(self, s):
         return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
 
+    # encrypts a message
     def encrypt(self, message, key, key_size=256):
         start = time.time()
         message = self.pad(message)
@@ -28,6 +33,7 @@ class Encryptor:
         encryptTimes.append(end - start)
         return ret
 
+    # encrypts a file
     def encrypt_file(self, file_name):
         with open(file_name, 'rb') as fo:
             plaintext = fo.read()
@@ -36,6 +42,7 @@ class Encryptor:
             fo.write(enc)
         os.remove(file_name)
 
+    # decrypts a message 
     def decrypt(self, ciphertext, key):
         start = time.time()
         iv = ciphertext[:AES.block_size]
@@ -47,6 +54,7 @@ class Encryptor:
         decryptTimes.append(end - start)
         return ret
 
+    # decrypts a file
     def decrypt_file(self, file_name):
         with open(file_name, 'rb') as fo:
             ciphertext = fo.read()
@@ -56,12 +64,13 @@ class Encryptor:
         os.remove(file_name)
 
 
+# encryption key
 key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
 enc = Encryptor(key)
 clear = lambda: os.system('cls')
 
 
-
+# runs encryption/decryption test 20 times
 for i in range(0,20):
 
     enc.encrypt_file("small.txt")
@@ -69,7 +78,7 @@ for i in range(0,20):
 
     enc.decrypt_file("small.txt.enc")
     
-
+# print test results
 print("average encrypt time: ", sum(encryptTimes)/len(encryptTimes))
 print("average decrypt time: ", sum(decryptTimes)/len(decryptTimes))
 
